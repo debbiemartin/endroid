819f75d071e7d8ef8572743a48764e12cc4bcb9f
Debbie Martin (debomart) <debomart@cisco.com>
2020-10-22 14:09:16 +0100

    Updating version and changelog

--- src/endroid/__init__.py
+++ src/endroid/__init__.py
@@ -27,7 +27,7 @@ from endroid.database import Database
 import endroid.manhole
 
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
 
 LOGGING_FORMAT = '%(asctime)-8s %(name)-20s %(levelname)-8s %(message)s'
 LOGGING_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
